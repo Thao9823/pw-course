79d5e9415aa1abb617d519b2adf80872d68b8aa7
thao9823 <utthao1508@gmail.com>
2025-11-23 16:28:52 +0700

    feat: add ex4.js

---
+++ tests/lesson-02/ex4.js
@@ -0,0 +1,7 @@
+const height = 160;
+const slheight = height - 100;
+let weight = slheight * 9 / 10;
+let weightmax = slheight;
+let weightmin = slheight * 8 / 10;
+console.log(weight, weightmax, weightmin);
+console.log ("cân nặng lý tưởng của bạn là:" + weight, "cân nặng tối đa là:"+ weightmax, "cân nặng tối thiểu là:" + weightmin);
