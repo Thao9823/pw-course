87829da2be94ecf8f08558248d87edb151375cd2
Thao <utthao1508@gmail.com>
2025-12-07 15:55:25 +0700

    feat: add test2.spec.ts

---
+++ tests/lesson-05/test2.spec.ts
@@ -0,0 +1,18 @@
+import {test} from "@playwright/test";
+test("Product page", async ({page}) => {
+    await page.goto ("https://material.playwrightvn.com/")
+
+    //di chuyển đến page product
+    await page.click ("//a[@href='02-xpath-product-page.html']");
+    
+    //thêm 2 - Sản phẩm 1:
+    await page.locator ("//button[@class='add-to-cart' and @data-product-id='1']").dblclick();
+    //await page.locator ("//button[contains(@class, 'add-to-cart') and @data-product-id='2']").dblclick();
+    
+    //thêm 3- sản phẩm 2:
+    for (let i = 1; i <= 3; i++){
+    await page.locator ("//button[@class='add-to-cart' and @data-product-id=2]").click();}
+
+    //thêm 1- Sản phẩm 3
+    await page.locator ("//button[contains(@class, 'add-to-cart') and @data-product-id='3']").click();
+});
