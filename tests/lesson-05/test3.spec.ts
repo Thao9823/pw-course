35d881aaac2a098b463a0cef10f0f8a27e885624
Thao <utthao1508@gmail.com>
2025-12-09 21:14:58 +0700

    fix: update test3.spec.ts

--- tests/lesson-05/test3.spec.ts
+++ tests/lesson-05/test3.spec.ts
@@ -3,16 +3,23 @@ test("Todo page", async ({ page }) => {
     await page.goto("https://material.playwrightvn.com/");
 
     //Di chuyển đến page TODO
-    await page.click("//a[@href='03-xpath-todo-list.html']");
+    await test.step("Redirect to page TODO", async () => {
+        await page.click("//a[@href='03-xpath-todo-list.html']");
+    })
 
     //Thêm mới 100 todo item có nội dung “Todo <i>”
-    for (let i = 1; i <= 100; i++) {
-        await page.locator("//input[@id='new-task']").fill("Todo " + i);
-        await page.locator("//button[@id='add-task']").click();
-    }
+    await test.step("Add 100 notes", async () => {
+        for (let i = 1; i <= 100; i++) {
+            await page.locator("//input[@id='new-task']").fill("Todo " + i);
+            await page.locator("//button[@id='add-task']").click();
+        }
+    })
+
     // Xoá các todo có số lẻ
-    for (let i = 1; i <= 100; i+= 2) {
-        page.once ('dialog', async dialog => dialog.accept());
-        await page.click(`//li[.//span[text()="Todo ${i}"]]//button[text()="Delete"]`);
-    }
+    await test.step("Remove note le", async () => {
+        for (let i = 1; i <= 100; i += 2) {
+            page.once('dialog', async dialog => dialog.accept());
+            await page.click(`//li[.//span[text()="Todo ${i}"]]//button[text()="Delete"]`);
+        }
+    })
 })
