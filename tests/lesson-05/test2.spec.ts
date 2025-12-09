4c6826bfdb2c655753478c0041e5122d6746731b
Thao <utthao1508@gmail.com>
2025-12-09 21:11:50 +0700

    fix: update tests.spec.ts

--- tests/lesson-05/test2.spec.ts
+++ tests/lesson-05/test2.spec.ts
@@ -3,17 +3,24 @@ test("Product page", async ({ page }) => {
     await page.goto("https://material.playwrightvn.com/")
 
     //di chuyển đến page product
+    await test.step("Redirect to Product Page", async() => {
     await page.click("//a[@href='02-xpath-product-page.html']");
+    })
 
     //thêm 2 - Sản phẩm 1:
+    await test.step("Add product 1", async () => {
     await page.locator("//button[@class='add-to-cart' and @data-product-id='1']").dblclick();
+    })
     //await page.locator ("//button[contains(@class, 'add-to-cart') and @data-product-id='2']").dblclick();
 
     //thêm 3- sản phẩm 2:
+    await test.step("Add product 2", async () => {
     for (let i = 1; i <= 3; i++) {
         await page.locator("//button[@class='add-to-cart' and @data-product-id=2]").click();
     }
-
+    })
     //thêm 1- Sản phẩm 3
+    await test.step("Add product 3", async () => {
     await page.locator("//button[contains(@class, 'add-to-cart') and @data-product-id='3']").click();
+    })
 });
